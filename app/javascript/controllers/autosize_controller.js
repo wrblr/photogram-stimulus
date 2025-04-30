8b154fe324ba49e5188ea505ec53c51d40b741fd
wrblr <wrbell@uchicago.edu>
2025-04-30 23:45:41 +0000

    autosize js controller working

---
+++ app/javascript/controllers/autosize_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import autosize from "autosize"
+
+// Connects to data-controller="autosize"
+export default class extends Controller {
+  connect() {
+    autosize(this.element)
+  }
+
+  disconnect() {
+    autosize.destroy(this.element)
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "jquery" # @3.7.1
+pin "autosize" # @6.0.1
